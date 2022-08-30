c74110695620e847bd6007d6ae1ecaad640b129f
Jon Jackson <ocskier@gmail.com>
2022-08-30 12:15:08 -0400

    Fixes formatting

--- server/src/main.py
+++ server/src/main.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request, jsonify, render_template
-from ariadne import load_schema_from_path, make_executable_schema,graphql_sync, snake_case_fallback_resolvers, ObjectType
+from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, ObjectType
 from ariadne.constants import PLAYGROUND_HTML
 
 from .schema.resolvers import getTime
