5a11ae18970b89022505e66d850546f86db2cf71
Jon Jackson <ocskier@gmail.com>
2022-09-03 09:44:35 -0500

    Adds mutation resolver

--- server/src/schema/resolvers.py
+++ server/src/schema/resolvers.py
@@ -1,12 +1,27 @@
 import time
 
+# Queries
 def getTime(obj, info):
     try:
-        # print(obj)
-        # print(info)
         payload = {
             "success": True,
             "time": time.time()
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
+        }
+    return payload
+
+# Mutations
+def connect(obj, info):
+    print(obj)
+    print(info)
+    try: 
+        payload = {
+            "success": True,
+            "msg": 'Successfully connected to the backend server!'
         }
     except Exception as error:
         payload = {
